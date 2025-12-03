c249f0b0122b592493279bb980d2aa2eacdfe2c4
olegbucolo <olegbucolo97@gmail.com>
2025-12-03 16:29:36 +0100

    style: agginti console.log a tutti gli esercizi

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -11,29 +11,36 @@ const teachers = [
 // 1. Utilizzando un indice sull'array teachers, cambia il valore della
 // variabile fourthTeacher qui sotto in modo che sia il quarto insegnante elencato nell'array
 const fourthTeacher = teachers[3];
+console.log(`1. fourthTeacher:`, fourthTeacher)
 
 // 2. Sostituisci il quinto insegnante nell'array teachers con 'Patrick'
 teachers.splice(4, 'Patrick')
+console.log(`2. Tolto Patrick. teachers:`, teachers);
 
 // 3. Rimuovi l'ultimo insegnante dall'array e salvalo nella variabile lastTeacher qui sotto
 const lastTeacher = teachers.pop();
+console.log(`3. lastTeacher:`, lastTeacher)
 
 // 4. Rimuovi il primo insegnante dall'array e salvalo nella variabile firstTeacher qui sotto
-const firstTeacher = teachers.splice(0, 1)
+const firstTeacher = teachers.shift();
+console.log(`4. firstTeacher:`, firstTeacher)
 
 // 5. Aggiungi un insegnante di nome 'Vanessa' alla fine dell'array teachers
 teachers.push('Vanessa')
+console.log(`2. Aggiunta Vanessa alla fine. teachers:`, teachers);
+
 
 // 6. Aggiungi un insegnante di nome 'Sarah' all'inizio dell'array teachers
 // teachers.
-teachers.splice(0, 0, 'Sara')
-console.log(`teachers`, teachers)
+teachers.unshift('Sara')
+console.log(`6. Aggiunta Sarah all'inizio. teachers:`, teachers)
 
 // 7. Trova l'indice dell'insegnante 'Lewis' nell'array teachers
 // e salvalo nella variabile lewisIndex
 const lewisIndex = teachers.indexOf('Lewis');
+console.log(`7. lewisIndex:`, lewisIndex)
 
 // 8. Verifica se l'array teachers è vuoto
 // e salva il risultato nella variabile isTeachersEmpty
-const isTeachersEmpty = teachers.length == 0 ? true : false;
-console.log(`ex: `, isTeachersEmpty)
+const isTeachersEmpty = teachers.length == 0;
+console.log(`8. isTeachersEmpty:`, isTeachersEmpty)
--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -10,22 +10,24 @@ const teachers = [
 
 // 1. Inverti l'ordine degli insegnanti nell'array teachers
 // e salva il risultato nella variabile reversedTeachers
-const reversedTeachers = null;
-
+const reversedTeachers = teachers.reverse();
+console.log(`1. reversedTeachers:`, reversedTeachers)
 
 // 2. Crea un nuovo array chiamato 'longNames' che contenga solo gli insegnanti
 // con un nome di lunghezza maggiore o uguale a 5 caratteri
-const longNames = null;
-for(let t in teachers){
-  if(t.length > 5) { longNames.push(t)} ;
-}
-console.log(longNames)
+const longNames = teachers.filter(t => t.length > 5)
+console.log(`2. longNames:`, longNames)
 
 // 3. Rimuovi 'Ed' dall'array teachers
+teachers.splice(teachers.indexOf('Ed'), 1)
+console.log(`3. teachers dopo rimozione di Ed:`, teachers)
 
 // 4. Verifica se 'Fabio' è presente nell'array teachers
 // e salva il risultato nella variabile isFabioPresent
-const isFabioPresent = null;
+const isFabioPresent = teachers.indexOf("Fabio") !== -1;
+console.log(`4. isFabioPresent:`,isFabioPresent)
 
-// 5. Unisci tutti gli insegnanti nell'array teachers in una stringa  separata da virgole e salvala nella variabile teachersString
-const teachersString = null;
+// 5. Unisci tutti gli insegnanti nell'array teachers in una stringa separata da virgole e 
+// salvala nella variabile teachersString
+const teachersString = teachers.join(', ');
+console.log(`5. teachersString:`, teachersString)
